f34047a0acf3d07116a4c483b712662ce5c16cd5
Matt LaGrandeur <mattlag@users.noreply.github.com>
2019-02-19 21:54:20 -0800

    Revert "test"
    This reverts commit c5be2eff4a9022a19618ff35dea4f4bff01f1297.

--- dev/objects/PlaceholderGlyph.js
+++ dev/objects/PlaceholderGlyph.js
@@ -225,8 +225,8 @@ export default class PlaceholderGlyph {
 }
 
 export const sampleGlyph12by20 = `
-0 0 0 0 0 0 0 0 0 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 0 
+1 0 0 0 0 0 0 0 0 0 0 0 
+1 1 0 0 0 0 0 0 0 0 0 0 
 0 1 1 0 0 0 0 0 0 0 0 0 
 0 0 1 1 0 0 0 0 0 0 0 0 
 0 0 0 1 1 0 0 0 0 0 0 0 
